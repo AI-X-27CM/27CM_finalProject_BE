a00ebd76d57c78e1231b5e14c3e0faed3567c0a0
djy2211 <131187694+djy2211@users.noreply.github.com>
2024-03-14 16:53:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile
 from typing import Optional
 import soundfile as sf
 import io
@@ -8,17 +8,19 @@ from sqlalchemy.orm import Session
 from typing import List
 from sqlalchemy import func
 from collections import defaultdict
-from datetime import datetime
 import json
 import uuid
 import shutil
 
 from database import Base, engine, SessionLocal
-from models import User, Detect, ALL
+from models import User, Detect, ALL, input_User
 import util
+
 import os
 from pydantic import BaseModel
 
+if not os.path.exists('uploads'):
+    os.makedirs('uploads')
 
 
 app = FastAPI()
@@ -32,15 +34,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-if not os.path.exists('uploads'):
-    os.makedirs('uploads')
-    
-
+# 전역 변수 초기화
+global_service_on = 0
+global_text_list = []
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return RedirectResponse(url="/docs")
 
+# 전화가 시작되었을 때, 아래 포인트에 get 요청을 보냄.
+@app.get("/start")
+async def start():
+    global global_service_on, global_text_list
+    global_service_on = 1
+    global_text_list = []
+    return {"message": "call start"}
+
+# 전화가 끝났을 때, 아래 엔드 포인트에 get 요청을 보냄
+@app.get("/end")
+async def end():
+    global global_service_on, global_text_list
+    global_service_on = 0
+    global_text_list = []
+    # 데이터베이스에 정보를 저장하는 로직을 추가
+    return {"message": "call end"}
 
 #월별 데이터 가져오기
 @app.get("/getMonthlyData/")
@@ -103,62 +120,49 @@ def get_pishingdata():
     return pishing_data
 
 
-@app.post("/STT")
-async def create_upload_file(file: UploadFile = File(...)):
-    contents = await file.read()
-    wav_data = util.convert_to_wav(contents)
-    bytes_io = io.BytesIO(wav_data)
-    data, samplerate = sf.read(bytes_io, dtype='float32')
-    if data.ndim > 1:
-        data = data[:, 0]
-    sand_text = util.whisper(data)
-    return sand_text
-
-
-
 # app에서 파일 업로드를 위한 API
 @app.post("/api")
 async def upload_file(file: UploadFile = File(...)):
-    unique_filename = f"{uuid.uuid4()}-{file.filename}"
-    file_path = f"./uploads/{unique_filename}"
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    return {"filename": unique_filename}
+    global global_service_on, global_text_list
+    global_service_on = 1 # api 테스트를 위해 활성화 필요
+    if global_service_on == 0:
+        return {"message": "not on call"}
+    else:
+        contents = await file.read()
+        unique_filename = f"{uuid.uuid4()}-{file.filename}"
+        file_path = f"./uploads/{unique_filename}"
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+            print(f"dddddfile{file.filename}")
 
+        wav_data = util.convert_to_wav(contents)
+        bytes_io = io.BytesIO(wav_data)
+        data, samplerate = sf.read(bytes_io, dtype='float32')
+        if data.ndim > 1:
+            data = data[:, 0]
+        sand_text = util.whisper(data)
+        global_text_list.append(sand_text)
+        
+        GPT_request = " ".join(global_text_list)
+        
+        answer = await util.gpt(GPT_request) # GPT 요청
 
-class TestUser(BaseModel):
-    id: str
-    pwd: str
-    phone: int
-    date: datetime
+        print(answer)
+        answer = json.loads(answer)
+        
+        if answer["label"] == "해당없음":
+            bool_label = 0
+        else:
+            bool_label = 1
 
+    return {"label": answer["label"], "bool_label": bool_label}
 
+# 회원가입 유저 정보 DB 저장
 @app.post("/addUser")
-async def add_user(user:TestUser):
-    print(user)
+async def add_user(user:input_User):
+    db = SessionLocal()
+    db.add(User(ID=user.id, PWD=user.pwd, Phone=user.phone, Date=user.date))
+    db.commit()
+    db.close()
     return {"message": "User added successfully", "user": user}
 
-
-# util.gpt('str변수') > GPT 모델 사용하는 함수
-
-# 서버단에서 테스트를 위해서 작성했던 코드
-# @app.post("/GPT")
-# async def create_gpt_answer(sand_text: str):
-#     answer = await util.gpt(sand_text)
-#     return answer
-
-
-# request 에 따른 응답 페이지 로직 예시
-# @app.post("/requestUser")
-# async def main_check(file: UploadFile = File(...), User_pk: int = Form(...)):
-#     sand_text = await create_upload_file(file) # Front(APP)에서 입력받은 파일을 쌓는 로직이 필요함
-#     answer = await create_gpt_answer(sand_text)
-#     if answer["label"] == "해당없음":
-#         return {"result": 0}
-#     else:
-#         Record = "url"
-#         db = SessionLocal()
-#         db.add(Detect(Date=datetime.now(), Record=Record, ID=User_pk, Label=answer["label"]))
-#         db.commit()
-#         db.close()
-#         return {"result": 1}
